f251b3b0eddf8a855e4b89d2510d7de77b74331d
Ahmad Mujamil <ahmad.mujamil@outlook.co.id>
2026-01-15 10:21:32 +0800

    menu sidebar & topbar

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,12 +4,15 @@
         <div class="container">
             <div class="row">
                 <div class="col-12 col-sm-6">
-                    <p class="mb-0 text-muted text-medium">MemoFit</p>
+                    <p class="mb-0 text-muted text-medium">Bansos Online System</p>
                 </div>
                 <div class="col-sm-6 d-none d-sm-block">
                     <ul class="breadcrumb pt-0 pe-0 mb-0 float-end">
                         <li class="breadcrumb-item mb-0 text-medium">
                             <a href="#" target="_blank" class="btn-link">Bantuan</a>
+                        </li>
+                        <li class="breadcrumb-item mb-0 text-medium">
+                            <a href="#" target="_blank" class="btn-link">Pemerintah Kabupaten Lombok Barat</a>
                         </li>
                         <li class="breadcrumb-item mb-0 text-medium"><a href="/" target="_blank" class="btn-link">&copy;2025  </a></li>
                         <li class="breadcrumb-item mb-0 text-medium">
