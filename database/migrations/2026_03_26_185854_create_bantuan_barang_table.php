7c0b7e2596808d9a86b4f337052ec0ff10aa3f91
Ahmad Mujamil <ahmad.mujamil@outlook.co.id>
2026-03-26 19:03:43 +0800

    add verfikasi pengajuan

---
+++ database/migrations/2026_03_26_185854_create_bantuan_barang_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bantuan_barang', function (Blueprint $table) {
+            $table->uuid("id")->primary();
+            $table->foreignUuid('verifikasi_pengajuan_id')->constrained("verifikasi_pengajuan");
+            $table->string('nama_barang');
+            $table->string('satuan');
+            $table->text('spesifikasi')->comment('format: ukuran, bahan, dll');
+            $table->decimal('harga_satuan', 18, 2);
+            $table->integer('qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bantuan_barang');
+    }
+};
